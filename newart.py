ca7f51b63b7a5135ae9519a9451ae9de357ac5d4
sweta jena <swetajena989@gmail.com>
2018-08-25 10:05:49 +0530

    add new shape

---
+++ newart.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+from turtle import *
+from random import *
+shape("turtle")
+speed(10)
+def randomcolor():
+  red = randint(0,255)
+  blue = randint(0,255)
+  green = randint(0,255)
+  color(red,green,blue)
+def randomplace():
+  x = randint(-100,100)
+  y = randint(-100,100)
+  goto(x,y)
+  pendown()
+def randomheading():
+  number = randint(1,360)
+  setheading(number)
+ 
+clear()
+setheading(0)
+def drawrectangle():
+  randomcolor()
+  randomplace()
+  hideturtle()
+  length = randint(5,100)
+  height=randint(5,100)
+  begin_fill()
+  forward(length)
+  right(90)
+  forward(height)
+  right(90)
+  forward(length)
+  right(90)
+  forward(height)
+  right(90)
+  end_fill()
+for i in range(10):
+    drawrectangle()
